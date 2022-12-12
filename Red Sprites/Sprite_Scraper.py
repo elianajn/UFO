fabb62b9dab053d6a9dfc587407717947dd6807b
elianajn <elianajneu@gmail.com>
2022-12-12 16:42:35 -0700

    removed unused imports and comments

--- Red Sprites/Sprite_Scraper.py
+++ Red Sprites/Sprite_Scraper.py
@@ -1,18 +1,12 @@
-from pkgutil import get_data
-from socket import timeout
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import csv
 import re
 from geopy.geocoders import Nominatim
-import geocoder
-from geopy.geocoders import ArcGIS
 import pickle
-import traceback
 from datetime import datetime
 
 class Sprite_Scraper():
@@ -21,26 +15,23 @@ class Sprite_Scraper():
         self.link = "https://spaceweathergallery.com/index.php?title=sprite"
         self.data = {}
         self.failed_geopy = {}
-        # self.list = pickle.load(open("sprites.p", "rb"))
 
     def scrape_pages(self):
         chrome_options = Options()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("ignore-certificate-errors")
         chrome_options.add_argument("user-data-dir=selenium")
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         s = Service('/usr/local/bin/chromedriver')
         driver = webdriver.Chrome(service=s,options=chrome_options)
         self.driver = driver
         while self.link is not None:
             self.driver.get(self.link)
             self.get_data()
-            # print(self.list)
             try:
                 self.link = self.driver.find_element(By.LINK_TEXT, 'next').get_attribute('href')
             except:
                 self.link = None
-        print("LENGTH BEFORE REMOVING NON-US: {}".format(len(self.data)))
         self.geopy()
         self.get_year_data()
         pickle.dump(self.failed_geopy, open('failed_sprites.p', 'wb'))
@@ -51,7 +42,6 @@ class Sprite_Scraper():
     def get_data(self):
         text = self.driver.page_source
         soup = BeautifulSoup(text, 'html.parser')
-        # entries = soup.find_all("div", {"class": "caption none"})
         rows = self.driver.find_elements(By.ID, 'sidebar')
         
         for row in rows:
@@ -62,32 +52,14 @@ class Sprite_Scraper():
                 sprite = {}
                 text = entry.text.split('\n')
                 sprite['Title'] = text[0]
-                # title = text[0]
                 sprite['Name'] = text[1]
                 sprite['month'] = text[2].split(" ")[0]
                 sprite['day'] = text[2].split(" ")[1]
                 sprite['Time']= text[2].split(" ")[2]
                 sprite['Location'] = text[3]
-                # print(entry.find_element(By.TAG_NAME, 'a').get_attribute('href'))
-                # entry.find_element(By.TAG_NAME, 'a').click()
                 link = entry.find_element(By.TAG_NAME, 'a').get_attribute('href')
                 self.data[link] = sprite
-                # print(link)
         
-
-        # return
-        # for sprite in self.data: # Need to go to the individual page for the year
-
-        #         print(sprite)
-        #         self.driver.get(sprite)
-        #         photoLocation = self.driver.find_element(By.CLASS_NAME, 'photoLocationText').text
-        #         # print(photoLocation)
-        #         year = re.findall(r'([0-9]{4})', photoLocation)[0]
-        #         print(self.data[sprite])
-        #         month = self.data[sprite].pop('month')
-        #         day = self.data[sprite].pop('day')
-        #         date = str(datetime.strptime("{} {} {}".format(month, day, year), '%b %d %Y')).split(" ")[0]
-        #         print(date)
 
     def get_year_data(self):
         for sprite in self.data:
@@ -132,6 +104,5 @@ class Sprite_Scraper():
 if __name__ == "__main__":
     scraper = Sprite_Scraper()
     scraper.scrape_pages()
-    print(len(scraper.data))
     
 
